4eb65b870a6fde310780f3dd2230aa991d3cdd36
MobTV <mobhater02@gmail.com>
2022-12-10 15:35:28 -0500

    Continued Setup for API Integration

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { buildCreateApi, createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '5b7c3a2c82mshf0157d6613214d9p1747c8jsn82b27bedde69');
+
+            return headers;
+        },
+    }),
+    endpoints: (buidler) => ({
+        getTopCharts: builder.query({ query: '/charts/world'}),
+    }),
+})
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi
